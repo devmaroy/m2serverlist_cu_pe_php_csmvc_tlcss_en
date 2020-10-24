3df0eb05d2e9798866beee0fa1c0d9cc657b4f23
Marek Matejovic <me@devmaroy.cz>
2020-10-24 12:35:36 +0200

    Update nav.php

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -4,7 +4,7 @@
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
 		</svg>
-		<span class="font-semibold text-xl tracking-tight">M2 Server List</span>
+		<span class="font-semibold text-xl tracking-tight"><a href="/">M2 Server List</a></span>
 	</div>
 	<div class="block lg:hidden">
 		<button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
